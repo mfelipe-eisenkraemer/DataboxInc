c59d76784a161e48da01005e3b73067d80a88faf
mfelipe-eisenkraemer <mfelipeme@gmail.com>
2015-08-10 20:51:13 +0100

    Initial commit

---
+++ app/src/main/java/project/course/qmul/databox/rest/RetrofitInterface.java
@@ -0,0 +1,30 @@
+package project.course.qmul.databox.rest;
+
+
+import com.google.gson.JsonElement;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Headers;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+/**
+ * Created by MateusFelipe on 16/06/2015.
+ */
+public interface RetrofitInterface {
+
+    // Asynchronously
+    @Headers({
+            "X-Mashape-Key : YOUR MASHAPE KEY",
+            "Content-Type: application/x-www-form-urlencoded",
+            "Accept: application/json"
+    })
+    @POST("/text/")
+    @FormUrlEncoded
+    void getSentiment(@Field("txt")String  tweetText, Callback<JsonElement> callback);
+}
